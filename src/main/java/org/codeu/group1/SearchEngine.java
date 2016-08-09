63b1f5bb5c8cd1662967aa37344a6a207426bb69
Russell Taber <rwtaber@gmail.com>
2016-08-09 18:41:04 -0400

    Testing word2vec

--- src/main/java/org/codeu/group1/SearchEngine.java
+++ src/main/java/org/codeu/group1/SearchEngine.java
@@ -1,10 +1,112 @@
 package org.codeu.group1;
 
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import redis.clients.jedis.Jedis;
+import org.jsoup.select.Elements;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.CollectionSentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.models.glove.Glove.Builder;
+import org.deeplearning4j.models.glove.Glove;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+
+import java.io.InputStream;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Locale;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class SearchEngine {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
+/*
+        String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
         Jedis jedis = new Jedis("100.111.160.126", 6379);
         jedis.auth("codeUgroup1");
+
+        JedisIndex index = new JedisIndex(jedis);
+
+        WikiCrawler wc = new WikiCrawler(source, index);
+        WikiFetcher wf = new WikiFetcher();
+*/
+
+        List<String> sentences = new ArrayList<>();
+        File gModel = new File("GoogleNews-vectors-negative300.bin.gz");
+        WordVectors old_vec = WordVectorSerializer.loadGoogleModel(gModel, true);
+
+/*        File dir = new File("src/main/resources");
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                InputStream stream = new FileInputStream(child);
+
+                Document doc = Jsoup.parse(stream, "UTF-8", "");
+                Element content = doc.getElementById("mw-content-text");
+                Elements paras = content.select("p");
+
+                String raw = paras.text().replaceAll("\\[.*?\\] ?", "");
+
+                BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+                iterator.setText(raw);
+
+                int start = iterator.first();
+                for (int end = iterator.next();
+                     end != BreakIterator.DONE;
+                     start = end, end = iterator.next()) {
+                    sentences.add(raw.substring(start, end));
+                }
+            }
+        }*/
+/*
+        Elements paragraphs = wf.fetchWikipedia(source);
+        String raw = paragraphs.text().replaceAll("\\[.*?\\] ?", "");
+
+        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+        iterator.setText(raw);
+
+        List<String> sentences = new ArrayList<>();
+        int start = iterator.first();
+        for (int end = iterator.next();
+             end != BreakIterator.DONE;
+             start = end, end = iterator.next()) {
+            sentences.add(raw.substring(start, end));
+        }
+
+        for(String sentence : sentences) {
+            System.out.println(sentence);
+        }
+
+        SentenceIterator iter = new CollectionSentenceIterator(sentences);
+
+        TokenizerFactory t = new DefaultTokenizerFactory();
+        t.setTokenPreProcessor(new CommonPreprocessor());
+
+
+
+        Glove new_vec = new Glove.Builder()
+                .minWordFrequency(5)
+                .iterations(1)
+                .layerSize(10)
+                .seed(42)
+                .windowSize(5)
+                .iterate(iter)
+                .tokenizerFactory(t)
+                .useExistingWordVectors(old_vec)
+                .build();
+
+        new_vec.fit();
+*/
+        System.out.println(old_vec.wordsNearest(Arrays.asList(""), Arrays.asList(""), 10));
     }
 }
---
+++ src/main/java/org/codeu/group1/WikiCrawler.java
@@ -0,0 +1,128 @@
+package org.codeu.group1;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+
+import redis.clients.jedis.Jedis;
+
+
+public class WikiCrawler {
+    // keeps track of where we started
+    private final String source;
+
+    // the index where the results go
+    private JedisIndex index;
+
+    // queue of URLs to be indexed
+    private Queue<String> queue = new LinkedList<String>();
+
+    // fetcher used to get pages from Wikipedia
+    final static WikiFetcher wf = new WikiFetcher();
+
+    private final static String site = "https://en.wikipedia.org";
+    /**
+     * Constructor.
+     *
+     * @param source
+     * @param index
+     */
+    public WikiCrawler(String source, JedisIndex index) {
+        this.source = source;
+        this.index = index;
+        queue.offer(source);
+    }
+
+    /**
+     * Returns the number of URLs in the queue.
+     *
+     * @return
+     */
+    public int queueSize() {
+        return queue.size();
+    }
+
+    /**
+     * Gets a URL from the queue and indexes it.
+     * @param b
+     *
+     * @return Number of pages indexed.
+     * @throws IOException
+     */
+    public String crawl(boolean testing) throws IOException {
+        String url = queue.remove();
+
+        if (index.isIndexed(url) && !testing) {
+            return null;
+        }
+
+        Elements url_p = testing ? wf.readWikipedia(url) : wf.fetchWikipedia(url);
+        index.indexPage(url, url_p);
+        queueInternalLinks(url_p);
+
+        return url;
+    }
+
+    /**
+     * Parses paragraphs and adds internal links to the queue.
+     *
+     * @param paragraphs
+     */
+    // NOTE: absence of access level modifier means package-level
+    void queueInternalLinks(Elements paragraphs) {
+        for (Element p : paragraphs) {
+            Iterable<Node> iter = new WikiNodeIterable(p);
+
+            for(Node node : iter) {
+                if (node instanceof TextNode) continue;
+
+                Element e = (Element) node;
+
+                if (e.tagName() == "a") {
+                    String href = e.attr("href").split("#")[0];
+                    String rel = e.attr("rel");
+
+                    if (!href.isEmpty() &&
+                            !href.contains("index.php") &&
+                            !rel.equals("nofollow")) {
+                        queue.add(site + href);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // make a WikiCrawler
+        Jedis jedis = JedisMaker.make();
+        JedisIndex index = new JedisIndex(jedis);
+        String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+        WikiCrawler wc = new WikiCrawler(source, index);
+
+        // for testing purposes, load up the queue
+        Elements paragraphs = wf.fetchWikipedia(source);
+        wc.queueInternalLinks(paragraphs);
+
+        // loop until we index a new page
+        String res;
+        do {
+            res = wc.crawl(false);
+
+            // REMOVE THIS BREAK STATEMENT WHEN crawl() IS WORKING
+            break;
+        } while (res == null);
+
+        Map<String, Integer> map = index.getCounts("the");
+        for (Entry<String, Integer> entry: map.entrySet()) {
+            System.out.println(entry);
+        }
+    }
+}
